38541d9d769482216887c4486056c375c62f196d
riganfauzi <rigan.fauzi@kinersia.com>
2023-03-31 09:39:02 +0700

    chose seat page

---
+++ lib/ui/pages/chose_seat_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:airplane/shared/theme.dart';
+
+class ChoseSeatPage extends StatelessWidget {
+  const ChoseSeatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Text(
+          'Select Your\n Favorite Seat',
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget seatStatus() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            // NOTE: AVAILABLE
+            Container(
+              width: 16,
+              height: 16,
+              margin: EdgeInsets.only(right: 6),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_available.png',
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Available',
+              style: blackTextStyle,
+            ),
+            // NOTE: SELECTED
+            Container(
+              width: 16,
+              height: 16,
+              margin: EdgeInsets.only(right: 6, left: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_selected.png',
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Selected',
+              style: blackTextStyle,
+            ),
+            // NOTE: UNAVAILABLE
+            Container(
+              width: 16,
+              height: 16,
+              margin: EdgeInsets.only(right: 6, left: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_unavailable.png',
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Unavailable',
+              style: blackTextStyle,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackGroundColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          title(),
+        ],
+      ),
+    );
+  }
+}
